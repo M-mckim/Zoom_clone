387f795b9d06d7f8ba7fb9b71aadda6cccd0df1e
yukino103 <M-mckim>
2023-03-17 10:12:18 +0900

    chatt_clear

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import { Socket } from 'dgram';
 import express from 'express'
+import http from 'http'
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -6,6 +9,36 @@ app.set("view engine", "pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+let sockets = [];
+
+
+wss.on("connection",(socket)=> {
+    socket["nickname"] = "Unknown";
+    sockets.push(socket);
+    console.log(socket);
+    socket.on("close", ()=> console.log("Disconnected from Browser"));
+    socket.on("message",(message)=>{
+        message = message.toString('utf8');
+        console.log(message)
+        const parsedMessage = JSON.parse(message);
+        switch(parsedMessage.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => {
+                    aSocket.send(`${socket.nickname}: ${parsedMessage.payload}`);
+                });
+                break;
+            case "nickname":
+                socket["nickname"] = parsedMessage.payload;
+                break;
+        }
+    });
+});
+
+server.listen(3000, handleListen);
+
